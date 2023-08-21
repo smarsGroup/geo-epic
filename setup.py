702bbadeb012a8c87830c55e82e0e9ddddfa5f76
Sachin Velmurugan <velmurugans@gsapp17.gshpc.umd.edu>
2023-08-21 14:31:37 -0400

    change package version

--- setup.py
+++ setup.py
@@ -2,9 +2,11 @@ from setuptools import setup
 
 setup(
     name='epic-util',
-    version="0.1",
-    url='https://github.com/smarsGroup/EPIC-utils',
+    url='https',
     author='Bharath Chandra Reddy Irigireddy',
     author_email='bharathc@umd.edu',
     packages=['epic_lib'],
+    install_requires=['numpy'],
+    version='0.5',
+    license='MIT',
 )
