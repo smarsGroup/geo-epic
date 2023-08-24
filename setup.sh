20a76cf4344dda1d501691687a0e180ea7a145e8
Sachin Velmurugan <velmurugans@gsapp17.gshpc.umd.edu>
2023-08-24 02:02:46 -0400

    change setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-if [$# -eq 0 ;] then
-    echo hello
+if [ $# -eq 0 ]; then
     wget "https://smarslab-files.s3.amazonaws.com/epic-utils/epic_pkg.zip"
 else
     wget -P $1 "https://smarslab-files.s3.amazonaws.com/epic-utils/epic_pkg.zip"
@@ -28,24 +27,24 @@ fi
 
 # Update the shebang line of scripts in the scripts directory
 cd ./epic_pkg
-/Users/sachinvel/Documents/geo-intern/epic-utils/setup.sh
-script="./epic_lib/dispatcher.py"
-# sed -i '/^#!/d' "$script"
+script="$(pwd)/epic_lib/dispatcher.py"
 echo "#!$interpreter_path" | cat - "$script" > temp && mv temp "$script"
 chmod +x "$script"
 
-wrapper_script="./epic_lib/dispatcher_wrapper.sh"
+
+wrapper_script="$(pwd)/epic_lib/scripts/epic_pkg"
+echo '#!/bin/bash \n '$script' "$@" ' > "$wrapper_script"
 chmod +x "$wrapper_script"
 
 # Install Python dependencies
 pip install -r requirements.txt
 
-# Determine if the terminal is bash 
+# Determine if the terminal is bash
 if [ "$SHELL" = "/bin/bash" ] || [ "$0" = "bash" ]; then
-    echo 'export PATH=$PATH:'$(pwd)/scripts >> ~/.bashrc
+    echo 'export PATH=$PATH:'$(pwd)/epic_lib/scripts >> ~/.bashrc
     echo "Setup for Bash complete!"
 else
-    echo "You are using shell: $SHELL. Please add the path $(pwd)/scripts to your PATH."
+    echo "You are using shell: $SHELL. Please add the path $(pwd)/epic_lib/scripts to your PATH."
 fi
 
 echo "Restart your terminal or source your profile for the changes to take effect!"
