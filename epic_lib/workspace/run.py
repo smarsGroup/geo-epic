3917ed665476b4820d091c7e905789ab1b5475b0
Bharath chandra <iambharathchandra@gmail.com>
2023-09-28 16:53:57 -0400

    Update run.py

--- epic_lib/workspace/run.py
+++ epic_lib/workspace/run.py
@@ -33,7 +33,6 @@ daily_weather = DailyWeather(weather["dir"], weather["start_date"], weather["end
 subprocess.Popen(f'chmod +x {model}', shell=True).wait()
 model = model.split('/')[-1]
 def process_model(row):
-    print(row)
     fid = row['FieldID']
     # Define paths using the config and base_dir
     new_dir = os.path.join(base_dir, 'sims', str(fid))
@@ -57,7 +56,6 @@ def process_model(row):
     
     for out_type in config['output_types']:
         out_file_loc = os.path.join(new_dir, f'{fid}.{out_type}')
-        print(out_file_loc)
         if os.path.exists(out_file_loc) and os.path.getsize(out_file_loc) > 0:
             shutil.move(out_file_loc, os.path.join(output_dir, f'{fid}.{out_type}'))
         else:
@@ -74,7 +72,5 @@ info_ls = info.to_dict('records')
 total = len(info_ls)
 min_ind, max_ind = config["Range"]
 min_ind, max_ind = int(min_ind*total), int(max_ind*total)
-#parallel_executor(process_model, info_ls[min_ind: max_ind], max_workers = config["num_of_workers"])
-process_model(info_ls[0])
-process_model(info_ls[1])
+parallel_executor(process_model, info_ls[min_ind: max_ind], max_workers = config["num_of_workers"])
 #shutil.rmtree(os.path.join(base_dir, 'sims'))
