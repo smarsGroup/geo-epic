104f8e017d3ee447c397b0740ba7f5accc6c1399
Bharath chandra <iambharathchandra@gmail.com>
2023-09-28 13:02:35 -0400

    Update run.py

--- epic_lib/workspace/run.py
+++ epic_lib/workspace/run.py
@@ -72,6 +72,6 @@ info_ls = info.to_dict('records')
 total = len(info_ls)
 min_ind, max_ind = config["Range"]
 min_ind, max_ind = int(min_ind*total), int(max_ind*total)
-parallel_executor(process_model, info_ls[min_ind: max_ind], max_workers = config["num_of_workers"])
-# process_model(info_ls[0])
+#parallel_executor(process_model, info_ls[min_ind: max_ind], max_workers = config["num_of_workers"])
+process_model(info_ls[0])
 shutil.rmtree(os.path.join(base_dir, 'sims'))
