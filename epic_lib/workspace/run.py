1c01fdad00e03f27994f43f051a690a2e9a72792
Bharath chandra <iambharathchandra@gmail.com>
2023-10-04 00:00:35 -0400

    Update run.py

--- epic_lib/workspace/run.py
+++ epic_lib/workspace/run.py
@@ -58,13 +58,13 @@ def process_model(row):
         out_file_loc = f'{fid}.{out_type}'
         if os.path.exists(out_file_loc) and os.path.getsize(out_file_loc) > 0:
             shutil.move(out_file_loc, os.path.join(output_dir, f'{fid}.{out_type}'))
-        # else:
-        #     raise Exception("Simulation Failed")
+        else:
+            raise Exception("Output Files not Found")
     else: os.remove(os.path.join(log_dir, f"{fid}.out"))
 
     # Return to the root directory and delete the simulation directory
     os.chdir(base_dir)
-    # shutil.rmtree(new_dir)
+    shutil.rmtree(new_dir)
 
 info = pd.read_csv('info.csv')
 info_ls = info.to_dict('records')
