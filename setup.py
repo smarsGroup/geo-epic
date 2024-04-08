096ceecf766a8e62eac35323aea18a5f26e0454d
Bharath chandra <iambharathchandra@gmail.com>
2024-04-07 17:31:27 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,32 +11,34 @@ def install_gdal(filename):
     subprocess.check_call(['pip', 'install', filename])
     os.remove(filename)
 
-# Check if the OS is Windows
-is_windows = sys.platform.startswith('win')
+# Check if the OS is Windows and exit if true
+if sys.platform.startswith('win'):
+    print("Installation not supported for Windows.")
+    sys.exit(1)
 
-# Download and install the appropriate GDAL wheel file based on the OS
-# if is_windows:
-#     install_gdal("GDAL-3.4.1-cp39-cp39-win_amd64.whl")
-# else:
-#     install_gdal("GDAL-3.4.1-cp39-cp39-manylinux_2_5_x86_64.manylinux1_x86_64.whl")
-    
-# Define metadata directory in the user's home folder and create it if it doesn't exist
+install_gdal("GDAL-3.4.1-cp39-cp39-manylinux_2_5_x86_64.manylinux1_x86_64.whl")
+
+# Define metadata directory in the user's home folder
 home_dir = os.path.expanduser("~")
 metadata_dir = os.path.join(home_dir, 'epic_pkg_metadata')
+
+# Check if the epic_pkg_metadata directory already exists, create it if it doesn't, 
+# and skip downloading files if it does.
 if not os.path.exists(metadata_dir):
     os.makedirs(metadata_dir)
+    # List of files to download
+    files_to_download = [
+        "https://smarslab-files.s3.amazonaws.com/epic-utils/slope_us.tif",
+        "https://smarslab-files.s3.amazonaws.com/epic-utils/SRTM_1km_US_project.tif",
+        "https://smarslab-files.s3.amazonaws.com/epic-utils/SSURGO.tif"
+    ]
 
-# # List of files to download
-# files_to_download = [
-#     "https://smarslab-files.s3.amazonaws.com/epic-utils/slope_us.tif",
-#     "https://smarslab-files.s3.amazonaws.com/epic-utils/SRTM_1km_US_project.tif",
-#     "https://smarslab-files.s3.amazonaws.com/epic-utils/SSURGO.tif"
-# ]
-
-# # Download the files to the metadata directory
-# for file_url in files_to_download:
-#     filename = os.path.join(metadata_dir, os.path.basename(file_url))
-#     urllib.request.urlretrieve(file_url, filename)
+    # Download the files to the metadata directory
+    for file_url in files_to_download:
+        filename = os.path.join(metadata_dir, os.path.basename(file_url))
+        urllib.request.urlretrieve(file_url, filename)
+else:
+    print(f"'{metadata_dir}' already exists, skipping file downloads.")
 
 subprocess.check_call(['pip', 'install', '--no-binary', ':all:', 'ruamel.yaml==0.16.12'])
 
@@ -72,4 +74,3 @@ setup(
         ],
     },
 )
-
