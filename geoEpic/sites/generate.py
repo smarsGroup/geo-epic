777b7eb29a1757796ff923da4e91d691d335ee0b
Bharath chandra <iambharathchandra@gmail.com>
2024-10-16 12:28:49 -0700

    generate sites

--- geoEpic/sites/generate.py
+++ geoEpic/sites/generate.py
@@ -79,6 +79,8 @@ print("writing site files")
 with open(f"{prefix}/template.sit", 'r') as f:
     template = f.readlines()
 
+
+out_dir = os.makedirs(out_dir, exist_ok=True)
 def write_site(row):
     with open(os.path.join(out_dir, f"{int(row['SiteID'])}.SIT"), 'w') as f:
         # Modify the template lines
